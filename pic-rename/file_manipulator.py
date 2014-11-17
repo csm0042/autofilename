9b7711416c77a9d8f2fdfcbaff88a58c14a10646
chris.maue <csmaue@gmail.com>
2014-11-17 17:05:49 -0600

    Fully functional w/gui

--- pic-rename/file_manipulator.py
+++ pic-rename/file_manipulator.py
@@ -21,16 +21,14 @@ class file_manipulator(object):
         self.dirToScan = str()
         self.logfile = logfile
         self.filelist = []
+        self.whitelist = []
         
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', 
                             filename=self.logfile, filemode='w')
         logging.info('[file_manipulator.__init__] Logger started')
         self.path = str()
 
-        self.filePath = str()
-        self.fileName = str()
-        self.nfileName = str()
-        self.newfile = str()
+        self.file_path = str()
 
         self.filename_wp_we = str()
         self.filename_wp_ne = str()
@@ -38,13 +36,13 @@ class file_manipulator(object):
         self.filename_np_ne = str()
         self.filename_ext = str()
 
-        self.nfilename_wp_we = str()
-        self.nfilename_wp_ne = str()
-        self.nfilename_np_we = str()
-        self.nfilename_np_ne = str()
-        self.nfilename_ext = str()
-        self.nfilename_wp_we_mem = str()
+        self.new_filename_wp_we = str()
+        self.new_filename_wp_ne = str()
+        self.new_filename_np_we = str()
+        self.new_filename_np_ne = str()
+        self.new_filename_ext = str()
 
+        self.new_filename_wp_we_mem = str()
 
         self.count = 0
         self.already = 0
@@ -65,9 +63,9 @@ class file_manipulator(object):
         self.basedir = self.basedir.replace('\\', '/')
         self.basedir = self.basedir.replace('\n', '')
         for file in os.listdir(self.basedir):
-            self.file_wp = os.path.join(self.basedir, file)
-            if os.path.isfile(self.file_wp):
-                self.filelist.append(self.file_wp)
+            self.filename_wp_we = os.path.join(self.basedir, file)
+            if os.path.isfile(self.filename_wp_we):
+                self.filelist.append(self.filename_wp_we)
             else:
                 pass
         return self.filelist
@@ -83,9 +81,9 @@ class file_manipulator(object):
         self.basedir = self.basedir.replace('\n', '')
         for root, dirs, files in os.walk(os.path.normcase(self.basedir)):
             for file in files:
-                self.file_wp = os.path.join(root, file)
-                if os.path.isfile(self.file_wp):
-                    self.filelist.append(self.file_wp)
+                self.filename_wp_we = os.path.join(root, file)
+                if os.path.isfile(self.filename_wp_we):
+                    self.filelist.append(self.filename_wp_we)
                 else:
                     pass
         return self.filelist
@@ -97,13 +95,13 @@ class file_manipulator(object):
 # Check file type against list of acceptable file-types to modify
 #######################################################################################################################
     def return_valid_file_ext(self, file, whitelist):
-        self.file = os.path.normcase(file)
+        self.filename_wp_we = os.path.normcase(file)
         self.whitelist = whitelist
 
-        self.fileRoot, self.fileExt = os.path.splitext(file)
-        self.fileExt = self.fileExt.replace('.', '')
+        self.filename_wp_ne, self.filename_ext = os.path.splitext(self.filename_wp_we)
+        self.filename_ext = self.filename_ext.replace('.', '')
 
-        if self.fileExt in self.whitelist:
+        if self.filename_ext in self.whitelist:
             return True
         else:
             return False
@@ -115,25 +113,27 @@ class file_manipulator(object):
 # Extract file attributes
 #######################################################################################################################
     def generate_filename(self, file):
-        self.file = os.path.normcase(file)
+        self.filename_wp_we = os.path.normcase(file)
 
-        self.fileRoot, self.fileExt = os.path.splitext(self.file)
-        self.filePath, self.fileName = os.path.split(self.fileRoot)
+        self.filename_wp_ne, self.filename_ext = os.path.splitext(self.filename_wp_we)
+        self.file_path, self.filename_np_ne = os.path.split(self.filename_wp_ne)
+        self.filename_ext = self.filename_ext.replace('.', '')
 
-        info = os.stat(self.file)
+        info = os.stat(self.filename_wp_we)
         utc_mod_time = time.gmtime(info.st_mtime)
 
-        self.nfileName = ('(' + str(time.strftime('%Y', utc_mod_time)) + '-'
+        self.new_filename_np_ne = ('(' + str(time.strftime('%Y', utc_mod_time)) + '-'
                           + str(time.strftime('%m', utc_mod_time)) + '-'
                           + str(time.strftime('%d', utc_mod_time)) + ')_'
                           + str(time.strftime('%H', utc_mod_time)) + 'h'
                           + str(time.strftime('%M', utc_mod_time)) + 'm'
                           + str(time.strftime('%S', utc_mod_time)) + 's_'
-                          + str(info.st_size) + 'b' + self.fileExt)
-
-        self.newfile = os.path.join(self.filePath, self.nfileName)
-
-        return self.newfile
+                          + str(info.st_size) + 'b')
+        
+        self.new_filename_np_we = self.new_filename_np_ne + '.' + self.filename_ext
+        self.new_filename_wp_we = os.path.join(self.file_path, self.new_filename_np_we)
+        
+        return self.new_filename_wp_we
 
 
 
@@ -142,14 +142,14 @@ class file_manipulator(object):
 # Increment filename
 #######################################################################################################################
     def increment_filename(self, file, index):
-        self.nfileName = os.path.normcase(file)
+        self.new_filename_wp_we = os.path.normcase(file)
         self.index = index
 
-        self.index += 1
-        self.fileRoot, self.fileExt = os.path.splitext(self.nfileName)
-        self.nfileName = self.fileRoot + '(' + str(self.index) + ')' + self.fileExt
-        self.newfile = os.path.normcase(self.nfileName)
+        self.new_filename_wp_ne, self.new_filename_ext = os.path.splitext(self.new_filename_wp_we)
+        self.new_filename_ext = self.new_filename_ext.replace('.', '')
+        self.new_filename_wp_we = self.new_filename_wp_ne + '(' + str(self.index) + ').' + self.new_filename_ext
+        self.new_filename_wp_we = os.path.normcase(self.new_filename_wp_we)
 
-        return self.newfile
+        return self.new_filename_wp_we
 
 
--- pic-rename/rename_script.py
+++ pic-rename/rename_script.py
@@ -12,12 +12,19 @@ class rename_script(object):
                             filename=self.logfile, filemode='w')
         logging.info('[rename_script.__init__] Logger started')
 
-        self.file = str()
+        self.filename_wp_we = str()
         self.newfile = str()
-        self.mainipulator_object = file_manipulator.file_manipulator(str(path), logfile)
+        self.manipulator_object = file_manipulator.file_manipulator(str(path), logfile)
         self.extOk = False
         self.include_sub_folders = True
         self.validExt = ['jpg', 'jpeg', 'bmp', 'png', 'gif']
+        self.new_filename_wp_we_mem = str()
+
+        self.total_count = int()
+        self.dir_count = int()
+        self.type_count = int()
+        self.modified_count = int()
+        self.already_count = int()
 
         self.run()
 
@@ -26,30 +33,108 @@ class rename_script(object):
     def run(self):
         if self.include_sub_folders == True:
             logging.info('[rename_script.run] Including sub-folders')
-            self.filelist = self.mainipulator_object.return_files_including_subs()
+            self.filelist = self.manipulator_object.return_files_including_subs()
         else:
             logging.info('[rename_script.run] NOT Including sub-folders')
-            self.filelist = self.mainipulator_object.return_files_root_only()
+            self.filelist = self.manipulator_object.return_files_root_only()
+            
+        
+        for self.filename_wp_we in self.filelist:
+            self.total_count += 1
 
-        for self.file in self.filelist:
-            logging.info('[rename_script.run] Checking: %s' % self.file)
-            self.extOk = self.mainipulator_object.return_valid_file_ext(os.path.normcase(self.file), self.validExt)
-
-            if self.extOk == True:
-                logging.info('[rename_script.run] Filetype is valid for conversion')
-                self.newfile = self.mainipulator_object.generate_filename(self.file)
-                logging.info('[rename_script.run] Ideal file name: %s' % self.newfile)
+            # Check to determine if this is a file (not a directory)
+            if os.path.isfile(self.filename_wp_we):
+                self.step_pointer = 1
             else:
-                logging.info('[rename_script.run] Filetype is NOT valid for conversion')
+                self.step_pointer = 991
 
-            if self.extOk == True and self.newfile != self.file:
-                logging.info('[rename_script.run] File needs to be re-named')
-            else:
-                logging.info('[rename_script.run] File name is already correct')
 
-            if (self.extOk == True) and (self.newfile != self.file) and (os.path.isfile(self.newfile) == True):
-                logging.info('[rename_script.run] Collision detected with existing file')
+            # Check file extension to see if file-type is in the whitelist
+            if self.step_pointer == 1:
+                logging.info('[rename_script.run] Checking file-type of: %s' % self.filename_wp_we)
+                self.extOk = self.manipulator_object.return_valid_file_ext(os.path.normcase(self.filename_wp_we),
+                                                                           self.validExt)
+                if self.extOk == True:
+                    logging.info('[rename_script.run] File-type is valid')
+                    self.step_pointer = 2
+                else:
+                    logging.info('[rename_script.run] File-type is NOT valid')
+                    self.step_pointer = 992
+
+
+            # Generate revised filename based upon current rule-set
+            if self.step_pointer == 2:
+                self.new_filename_wp_we = self.manipulator_object.generate_filename(self.filename_wp_we)
+                if self.new_filename_wp_we == self.filename_wp_we:
+                    self.step_pointer = 3
+                else:
+                    logging.info('[rename_script.run] File-name is NOT valid')
+                    self.step_pointer = 4
+
+
+            # Check to see if file-names match because they are the same file
+            if self.step_pointer == 3:
+                if os.path.samefile(self.new_filename_wp_we, self.filename_wp_we):
+                    logging.info('[rename_script.run] File-name is valid')
+                    self.step_pointer = 993
+                else:
+                    logging.info('[rename_script.run] File-name is NOT valid')
+                    self.step_pointer = 4
+
+
+            # Check to see if desired file-name is already in-use
+            if self.step_pointer == 4:
+                if os.path.isfile(self.new_filename_wp_we) == True:
+                    logging.info('[rename_script.run] Name collision detected with existing file')
+                    self.step_pointer = 5
+                else:
+                    logging.info('[rename_script.run] Desired file-name is available')
+                    self.step_pointer = 6
+
+
+            # Add index number to tail-end of filename to make unique
+            if self.step_pointer == 5:
                 self.index = 0
-                while os.path.isfile(self.newfile) == True:
-                    self.newfile = self.mainipulator_object.increment_filename(self.newfile, self.index)
-                logging.info('[rename_script.run] Using instead: %s' % self.newfile)
+                self.new_filename_wp_we_mem = self.new_filename_wp_we
+                while os.path.isfile(self.new_filename_wp_we) == True:
+                    self.index += 1
+                    self.new_filename_wp_we = self.manipulator_object.increment_filename(self.new_filename_wp_we_mem,
+                                                                                         self.index)
+                logging.info('[rename_script.run] Next available file-name: %s' % self.new_filename_wp_we)
+                self.step_pointer = 6
+
+
+            # Perform file rename operaton
+            if self.step_pointer == 6:
+                os.rename(self.filename_wp_we, self.new_filename_wp_we)
+                logging.info('[rename_script.run] File rename operation successful')
+                self.step_pointer = 7
+
+
+            # Increment counters for logger
+            if self.step_pointer == 7:
+                self.modified_count += 1
+                self.step_pointer = 9999
+
+            if self.step_pointer == 991:
+                self.dir_count += 1
+                self.step_pointer = 9999
+
+            if self.step_pointer == 992:
+                self.type_count += 1
+                self.step_pointer = 9999
+
+
+            if self.step_pointer == 993:
+                self.already_count += 1
+                self.step_pointer = 9999
+
+        logging.info('[rename_script.run] RESULTS')
+        logging.info('[rename_script.run] %d Total objects' % self.total_count)
+        logging.info('[rename_script.run] %d Total objects modified' % self.modified_count)
+        logging.info('[rename_script.run] %d Total objects skipped because they are not files' % self.dir_count)
+        logging.info('[rename_script.run] %d Total objects skipped due to invalid type' % self.type_count)
+        logging.info('[rename_script.run] %d Total objects skipped because they are already properly formatted'
+                     % self.already_count)
+
+        
