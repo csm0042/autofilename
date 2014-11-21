739ae6948d794cb1e50ad70db6d90fd821bc6ef8
chris.maue <csmaue@gmail.com>
2014-11-20 19:32:08 -0600

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,99 @@
-try:
-	from setuptools import setup
-except ImportError:
-	from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+import os
 
-config = {
-	'description': 'Automatic File-name assigner',
-	'author': 'Chris Maue',
-	'author_email': 'csmaue@gmail.com',
-    'url': 'https://github.com/csm0042/autofilename.git',
-    'download_url': 'https://github.com/csm0042/autofilename.git',
-	'version': '1.0.0',
-	'packages': ['autofilename'],
-	'name': 'auto-file-name'
-}
-setup(**config)
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'autofilename',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
+    version = '1.2.0',
+
+    # Project description
+    description = 'Auto File-Rename Utility',
+    long_description = 'a utility to scan a folder and auto-rename files based upon their modified date and size',
+
+    # Project homepage
+    url = 'https://github.com/csm0042/autofilename.git',
+
+    # Author details
+    author = 'Christopher Maue',
+    author_email = 'csmaue@gmail.com',
+
+    # Choose your license
+    license = 'MIT',
+
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords = 'file utility',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages = find_packages(exclude=['_main', 'docs', 'test*']),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=[''],
+
+    # List additional groups of dependencies here (e.g. development dependencies).
+    # You can install these using the following syntax, for example:
+    # $ pip install -e .[dev,test]
+    extras_require = {
+    'dev': ['check-manifest'],
+    'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'sample': ['package_data.dat'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'gui = gui_builder.gui',
+        ],
+        'gui_scripts': [
+            'gui_callbacks = gui_callbacks:callback'
+        ]
+    },
+)
